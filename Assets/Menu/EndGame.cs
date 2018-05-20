5a2bc46e09d1fa3cc638e297f06a137795527d62
Unity <teddgiles@mail.com>
2018-05-20 07:50:44 +0100

    Initial Commit
    Transportation from local file to GIT

---
+++ Assets/Menu/EndGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour {
+
+
+	void OnCollisionEnter (Collision other)
+	{
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ Assets/Sounds/onColEnterSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class onColEnterSound : MonoBehaviour 
+{
+
+	public AudioClip ObjectHit_Surface;
+
+	void Start ()
+	{
+		GetComponent<AudioSource> ().playOnAwake = false;
+
+		GetComponent<AudioSource> ().clip = ObjectHit_Surface;
+	}
+
+	void onCollisionEnter(Collision other)
+	{
+		GetComponent<AudioSource> ().Play ();
+	}
+
+}
